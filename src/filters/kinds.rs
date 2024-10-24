acbd6d93519bb567792111083b5a1a8e8b564056
David Caseria <david@team.sovereign.app>
2024-10-24 12:30:43 -0400

    Modify kinds filter to block all but allowed

--- src/filters/kinds.rs
+++ src/filters/kinds.rs
@@ -4,23 +4,30 @@ use std::collections::HashMap;
 
 #[derive(Deserialize, Default)]
 pub struct Kinds {
-    kinds: Vec<i64>,
+    allowed_kinds: Option<Vec<i64>>,
+    blocked_kinds: Option<Vec<i64>>,
     messages: Option<HashMap<String, String>>,
 }
 
 impl NoteFilter for Kinds {
     fn filter_note(&mut self, input: &InputMessage) -> OutputMessage {
         let kind = input.event.kind;
-        if self.kinds.contains(&kind) {
-            let msg = self
-                .messages
-                .as_ref()
-                .and_then(|msgs| msgs.get(&kind.to_string()).cloned())
-                .unwrap_or_else(|| "blocked: note kind is not allowed here".to_string());
-            OutputMessage::new(input.event.id.clone(), Action::Reject, Some(msg))
-        } else {
-            OutputMessage::new(input.event.id.clone(), Action::Accept, None)
+        let msg = self
+            .messages
+            .as_ref()
+            .and_then(|msgs| msgs.get(&kind.to_string()).cloned())
+            .unwrap_or_else(|| "blocked: note kind is not allowed here".to_string());
+        if let Some(allowed_kinds) = &self.allowed_kinds {
+            if !allowed_kinds.contains(&kind) {
+                return OutputMessage::new(input.event.id.clone(), Action::Reject, Some(msg));
+            }
         }
+        if let Some(blocked_kinds) = &self.blocked_kinds {
+            if blocked_kinds.contains(&kind) {
+                return OutputMessage::new(input.event.id.clone(), Action::Reject, Some(msg));
+            }
+        }
+        OutputMessage::new(input.event.id.clone(), Action::Accept, None)
     }
 
     fn name(&self) -> &'static str {
